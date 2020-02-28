3d9815b0b41ec271c0d0c1d106092410b2732b78
SamuelDSR <samuel.longshihe@gmail.com>
2020-02-28 20:47:57 +0100

    add option first_only|force_integer, optimize

--- good_sum.py
+++ good_sum.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import copy
+import functools
 import itertools
+import operator
 import pprint
 import time
-import functools
+
 from eval_stack import signature
 
 ADD_FORMATTER = lambda a, b: '({}+{})'.format(a, b)
@@ -24,7 +26,6 @@ def timeit(func):
         print("Running time of {}: {:.2f} secs".format(
             func.__name__, end - start))
         return ret
-
     return _wrapper
 
 
@@ -54,41 +55,44 @@ def partitions(numbers):
                 yield list(part1), list(part2)
 
 
-def possible_two(a, b, a_rep=None, b_rep=None):
+def possible_two(a, b, force_integer):
     '''
     Possible result of two numbers using +|-|*|/ operators
     '''
-    a_rep = a_rep if a_rep is not None else a
-    b_rep = b_rep if b_rep is not None else b
-
     yield a * b, MUL_FORMATTER
     yield a + b, ADD_FORMATTER
     yield a - b, SUB1_FORMATTER
     yield b - a, SUB2_FORMATTER
-    if a != 0:
+    if a != 0 and (not force_integer or int(b / a) == b / a):
         yield b / a, DIV2_FORMATTER
-    if b != 0:
+    if b != 0 and (not force_integer or int(a / b) == a / b):
         yield a / b, DIV1_FORMATTER
 
 
-def possible_right(target, left):
+def possible_right(target, left, force_integer):
     '''
     Possible right value of [left +|-|*|/ right = target]
     '''
-    yield target - left, ADD_FORMATTER
-    if left != 0:
-        yield target / left, MUL_FORMATTER
-    yield left - target, SUB1_FORMATTER
-    yield left + target, SUB2_FORMATTER
-    # left/b = target, b can not be zero either.
-    if target != 0 and left != 0:
-        yield left / target, DIV1_FORMATTER
     # b/left = target, left can not be zero
     if left != 0:
         yield left * target, DIV2_FORMATTER
+    if left != 0 and (not force_integer
+                      or int(target / left) == target / left):
+        yield target / left, MUL_FORMATTER
+    # left/b = target, b can not be zero either.
+    if target != 0 and left != 0 and (not force_integer
+                                      or int(left / target) == left / target):
+        yield left / target, DIV1_FORMATTER
+    yield left - target, SUB1_FORMATTER
+    yield left + target, SUB2_FORMATTER
+    yield target - left, ADD_FORMATTER
 
 
-def possible_ways(numbers, target=None, memory=None):
+def possible_ways(numbers,
+                  target=None,
+                  memory=None,
+                  first_only=False,
+                  force_integer=False):
     """
     Possible ways using numbers, +|-|*|/, and brackets to get target.
     If target is None, return all possible values.
@@ -103,6 +107,10 @@ def possible_ways(numbers, target=None, memory=None):
             return memory[num_key]
 
     solutions = []
+    if target is not None and functools.reduce(operator.mul, numbers) < target:
+        if memory is not None:
+            memory[((numbers[0]), target)] = solutions
+        return solutions
 
     if len(numbers) == 1 and (numbers[0] == target or target is None):
         solutions = [(numbers[0], '({})'.format(numbers[0]))]
@@ -112,9 +120,13 @@ def possible_ways(numbers, target=None, memory=None):
 
     if len(numbers) == 2:
         a, b = numbers
-        for op_ret, op_formatter in possible_two(a, b):
-            if op_ret == target or target is None:
+        for op_ret, op_formatter in possible_two(a, b, force_integer):
+            if target is None or (op_ret == target and not first_only):
                 solutions.append((op_ret, op_formatter(a, b)))
+            elif op_ret == target and first_only:
+                return [(op_ret, op_formatter(a, b))]
+            else:
+                pass
         if memory is not None:
             num_key = (tuple(sorted(numbers)), target)
             memory[num_key] = solutions
@@ -129,16 +141,23 @@ def possible_ways(numbers, target=None, memory=None):
                 right_ways = possible_ways(right, target, memory=memory)
                 for right_ret, right_repr in right_ways:
                     for op_ret, op_formatter in possible_two(
-                            left_ret, right_ret):
+                            left_ret, right_ret, force_integer):
                         solutions.append(
                             (op_ret, op_formatter(left_repr, right_repr)))
             else:
                 for right_ret, op_formatter in possible_right(
-                        target, left_ret):
+                        target, left_ret, force_integer):
                     right_ways = possible_ways(right, right_ret, memory=memory)
                     for right_ret, right_repr in right_ways:
-                        solutions.append(
-                            (target, op_formatter(left_repr, right_repr)))
+                        if first_only:
+                            if memory is not None:
+                                num_key = (tuple(sorted(numbers)), target)
+                                memory[num_key] = solutions
+                            return [(target,
+                                     op_formatter(left_repr, right_repr))]
+                        else:
+                            solutions.append(
+                                (target, op_formatter(left_repr, right_repr)))
     if memory is not None:
         num_key = (tuple(sorted(numbers)), target)
         memory[num_key] = solutions
@@ -146,15 +165,24 @@ def possible_ways(numbers, target=None, memory=None):
 
 
 if __name__ == '__main__':
-    # search all solutions without memory
-    solutions = timeit(possible_ways)([10, 7, 5, 5, 6, 2, 1], 1694, None)
-    pprint.pprint(solutions)
+    numbers = [10, 7, 5, 5, 2, 1]
+    target = 648
+    print("========================================================")
+    print("Search first solution without memory:")
+    pprint.pprint(timeit(possible_ways)(numbers, target, None, first_only=True))
 
-    # search all solutions with memory
-    solutions = timeit(possible_ways)([10, 7, 5, 5, 6, 2, 1], 1694, {})
+    print("========================================================")
+    print("Search first solution with memory:")
+    pprint.pprint(timeit(possible_ways)(numbers, target, {}, first_only=True))
+
+    print("========================================================")
+    print("Search all solutions with memory:")
+    solutions = timeit(possible_ways)(numbers, target, {}, first_only=False)
     pprint.pprint(solutions)
 
     # remove all equivalent solutions
     solutions_dict = dict((signature(s[1]), s[1]) for s in solutions)
     solutions = list(solutions_dict.values())
+    print("========================================================")
+    print("Unique solutions:")
     pprint.pprint(solutions)
